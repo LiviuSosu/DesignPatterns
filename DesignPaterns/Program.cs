704ceccff87ba1e192e98bdc62274363249ec2de
Liviu Şoşu <sosuliviu@gmail.com>
2018-11-11 08:25:45 +0200

    added composite

--- DesignPaterns/Program.cs
+++ DesignPaterns/Program.cs
@@ -5,6 +5,7 @@ using DesignPaterns.Creational.Prototype;
 using DesignPaterns.Creational.Singleton;
 using DesignPaterns.Structural.Adapter;
 using DesignPaterns.Structural.Bridge;
+using DesignPaterns.Structural.Composite;
 using System;
 
 namespace DesignPaterns
@@ -23,7 +24,8 @@ namespace DesignPaterns
 
             #region Structural design patterns
             //Adapter();
-            Bridge();
+            //Bridge();
+            Composite();
             #endregion
 
             #region Behavioral design patterns
@@ -170,6 +172,40 @@ namespace DesignPaterns
             customers.Add("Henry Velasquez");
 
             customers.ShowAll();
+        }
+
+        static void Composite()
+        {
+            // Create a tree structure 
+
+            CompositeElement root =
+              new CompositeElement("Picture");
+            root.Add(new PrimitiveElement("Red Line"));
+            root.Add(new PrimitiveElement("Blue Circle"));
+            root.Add(new PrimitiveElement("Green Box"));
+
+            // Create a branch
+
+            CompositeElement comp =
+              new CompositeElement("Two Circles");
+            comp.Add(new PrimitiveElement("Black Circle"));
+            comp.Add(new PrimitiveElement("White Circle"));
+            root.Add(comp);
+
+            // Add and remove a PrimitiveElement
+
+            PrimitiveElement pe =
+              new PrimitiveElement("Yellow Line");
+            root.Add(pe);
+            root.Remove(pe);
+
+            // Recursively display nodes
+
+            root.Display(1);
+
+            // Wait for user
+
+            Console.ReadKey();
         }
     }
 }
